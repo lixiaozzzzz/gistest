0b22f7571ed0105650a4b6dc53bae02de52ad26c
lixiao <lixiao@jixiang.cn>
2024-10-17 16:58:45 +0800

    fix: base测试

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import Map from "ol/Map.js";
-import OSM from "ol/source/OSM.js";
+// import OSM from "ol/source/OSM.js";
 import XYZ from "ol/source/XYZ.js";
 import TileLayer from "ol/layer/Tile.js";
 import View from "ol/View.js";
-
+import { fromLonLat } from "ol/proj";
+import { ZoomToExtent } from "ol/control";
 const map = new Map({
   layers: [
     new TileLayer({
@@ -11,14 +12,22 @@ const map = new Map({
         url: "http://wprd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}",
       }),
     }),
+    // new TileLayer({
+    //   source: new OSM(),
+    // }),
   ],
   target: "map",
   view: new View({
-    center: [12914838.35, 4814529.9],
-    zoom: 8,
-    maxZoom: 18,
-    projection: "EPSG:3857",
+    center: fromLonLat([125.32, 43.88]),
+    zoom: 4,
+    // maxZoom: 18,
+    // projection: "EPSG:3857",
     constrainResolution: true, // 设置缩放级别为整数
     smoothResolutionConstraint: false, // 关闭无级缩放地图
   }),
 });
+
+const zoomToExtent = new ZoomToExtent({
+  extent: [110, 30, 160, 30],
+});
+map.addControl(zoomToExtent);
